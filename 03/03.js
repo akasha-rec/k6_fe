7f9152a805e728311f08866ed44ad9211125976d
akasha-rec <uspira@naver.com>
2024-02-22 13:10:01 +0900

    메시지 출력 방법 if

--- 03/03.js
+++ 03/03.js
@@ -2,7 +2,12 @@
 // 변수를 선언할 때 
 
 const handleClick =(n)=> {
-    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n}이 눌러졌습니다</h2>`;
+    let msg;
+    if (n==1) msg = "<h2>안녕하세요.</h2>";
+    else msg = "<h2>안녕히 가세요</h2>";
+    document.querySelector("#msgArea").innerHTML = msg;
+
+    // document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n}이 눌러졌습니다</h2>`;
 }
 let(변수)
 // 함수 작성 1
