27f7d131ec4a280c5053ef6f3c3c2e9fe5101180
akasha-rec <uspira@naver.com>
2024-02-23 17:33:28 +0900

    각각 주사위 다르게 굴리기

--- 04/04.js
+++ 04/04.js
@@ -11,12 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const handleClick = () => { //함수 호출
-    const n = Math.floor(Math.random() * 6) + 1;
+    //floor : 버림, (Math.random() * 6) : 0~5 > +1 > 1~6
+    for(let i=1; i<=3; i++) {
+    const n = Math.floor(Math.random() * 6) + 1; 
     //이미지에 src 바꿔주기
-    const img = document.querySelector("#img");//셀렉터 찾기 = 적용 대상 찾기
+    const img = document.querySelector(`#img${i}`);//셀렉터 찾기 = 적용 대상 찾기
     img.setAttribute("src", `./img/${n}.png`) //속성(src)를 바꿔서 이미지(링크)를 바꿔준다
     console.log("Click", n); // 버튼을 누를 때마다 n이 생긴다
 }
+};
 
 // 짜냈다...
 // const $random = document.querySelector("#random1");
