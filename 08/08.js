a3c0880d8f9fb1cbdde7b58ebbb248df2b2aac84
akasha-rec <uspira@naver.com>
2024-03-05 11:40:59 +0900

    업다운 게임 끝

--- 08/08.js
+++ 08/08.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
     //const numInput = document.getElementById("# 쓰면 안 돼")
     const bt = document.querySelector("button");
     const msg = document.querySelector("#msg");
+    
+    //inputbox focus
+    numInput.focus();
 
     //랜덤수 생성 변수
     let n; //undefined type
@@ -13,15 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
     //button click event
     bt.addEventListener("click", (e) => {
         e.preventDefault();
-
-        //if(flag === false) {
-        if(!flag) {
-        n = Math.floor(Math.random()*100) +1; //1~100
-        console.log("n=", n);
-        flag = true;
-        }
-
+        
         //input 박스 내용 가져오기
+        //(parseInt(numInput.value < 1 || numInput.value < 100)
         if(numInput.value == "") {
             //alert("숫자를 입력하세요"); inputbox에 값이 없으면 백엔드로 데이터 보내지X
             document.querySelector("#msg").innerHTML="<span>숫자를 입력하세요</span>";
@@ -29,16 +26,33 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
         }
 
+        //if(flag === false) {
+        if(!flag) {
+        n = Math.floor(Math.random()*100) +1; //1~100
+        console.log("n=", n);
+        flag = true;
+        
+        //초기화
+        numInput.style.display = "inline";
+        bt.innerHTML = "확인";
+        numInput.value = "";
+        numInput.focus;
+        bt.innerHTML = "확인";
+        }
+
         //숫자비교
         msg.innerHTML="";
         if (n === parseInt(numInput.value)) {
-            upDownImg.setAttribute("src", "./imges/good.png")
+            upDownImg.setAttribute("src", "./imges/good.png");
+            numInput.style.display = "none"; //CSS 제어 가능하다
+            bt.innerHTML = "재시작"
+            flag = false; //초기값으로 다시
         }
         else if (n > numInput.value) {
-            upDownImg.setAttribute("src", "./imges/up.png")
+            upDownImg.setAttribute("src", "./imges/up.png");
         }
         else {
-            upDownImg.setAttribute("src", "./imges/down.png")
+            upDownImg.setAttribute("src", "./imges/down.png");
         }
     });
 });
